3d7b21412ca19b6e4561ab7093021063fa99be6a
vrushank <vrushank1729@gmail.com>
2023-12-06 21:17:42 +0530

    Create display(). When btn clicked, tis displayed on screen

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ function divide(a, b) {
     return a / b
 }
 
+let num1, operator, num2
+
+function operate(num1, operate, num2) {
+    if(operate === '+') {
+        return add(num1, num2)
+    } else if(operate === '-') {
+        return subtract(num1, num2)
+    } else if(operate === '*') {
+        return multiply(num1, num2)
+    } else if(operate === '/') {
+        return divide(num1, num2)
+    }
+}
+
+function display() {
+    let btns = document.querySelectorAll('.btn')
+    btns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            let btn_text = btn.textContent
+            let clickDisplay = document.querySelector('.clickDisplay')
+            if(btn_text === 'clear') {
+                clickDisplay.textContent = ''
+            } else if(btn_text !== '=') {
+                clickDisplay.textContent += btn_text
+            }
+        })
+    })
+}
+
+display()
