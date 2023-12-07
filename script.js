1caf9e6c4e1fd833884b09ab27f022c34e98cfd9
vrushank <vrushank1729@gmail.com>
2023-12-08 00:21:43 +0530

    Calculator that works fine with natural numbers

--- script.js
+++ script.js
@@ -1,47 +1,12 @@
-function add(a, b) {
-    return a + b
-}
-
-function subtract(a, b) {
-    return a - b
-}
-
-function multiply(a, b) {
-    return a * b
-}
-
-function divide(a, b) {
-    return a / b
-}
-
 function operate(num1, operator, num2) {
     if(operator === '+') {
-        return add(num1, num2)
+        return num1 + num2
     } else if(operator === '-') {
-        return subtract(num1, num2)
+        return num1 - num2
     } else if(operator === '*') {
-        return multiply(num1, num2)
+        return num1 * num2
     } else if(operator === '/') {
-        return divide(num1, num2)
-    }
-}
-
-function display(btn_text) {
-    let clickDisplay = document.querySelector('.clickDisplay')
-    let displayText = clickDisplay.textContent, n = displayText.length
-
-    if(btn_text === 'clear') {
-        clickDisplay.textContent = ''
-    }
-    else if(!isNaN(btn_text)) {
-        clickDisplay.textContent += btn_text
-    }
-    else if( (btn_text !== '=') && (displayText.length !== 0) ) {
-        if(isNaN(displayText[n-1])) {
-            clickDisplay.textContent = displayText.substring(0, n-1) + btn_text
-        } else {
-            clickDisplay.textContent += btn_text
-        }
+        return num1 / num2
     }
 }
 
@@ -50,7 +15,35 @@ function calculate() {
     btns.forEach((btn) => {
         btn.addEventListener('click', () => {
             let btn_text = btn.textContent
-            display(btn_text)
+            let clickDisplay = document.querySelector('.clickDisplay')
+            let displayText = clickDisplay.textContent, n = displayText.length
+
+            if(!isNaN(btn_text)) {
+                clickDisplay.textContent += btn_text
+            }
+            else if(btn_text === 'clear') {
+                clickDisplay.textContent = ''
+            }
+            else if(btn_text !== '=' && (n>0) && !isNaN(displayText.substring(0, n-1))) {
+                if(isNaN(displayText[n-1])) {
+                    clickDisplay.textContent = displayText.substring(0, n-1) + btn_text
+                } else {
+                    clickDisplay.textContent += btn_text
+                }
+            }
+            else {
+                let operators = ['+', '-', '*', '/']
+                for(let i=0; i<4; i++) {
+                    if(displayText.includes(operators[i])) {
+                        let j = displayText.indexOf(operators[i])
+                        let num1 = parseInt( displayText.substring(0, j), 10 )
+                        let num2 = parseInt( displayText.substring(j+1, n), 10 )
+
+                        let result = operate(num1, operators[i], num2)
+                        clickDisplay.textContent = result + btn_text
+                    }
+                }
+            }
         })
     })
 }
