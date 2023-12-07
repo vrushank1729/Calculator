643e79510459b775ac713ebd50f2566fb3be43a5
vrushank <vrushank1729@gmail.com>
2023-12-07 21:22:40 +0530

    make sure in display that operator not followed by operator

--- script.js
+++ script.js
@@ -14,33 +14,45 @@ function divide(a, b) {
     return a / b
 }
 
-let num1, operator, num2
-
-function operate(num1, operate, num2) {
-    if(operate === '+') {
+function operate(num1, operator, num2) {
+    if(operator === '+') {
         return add(num1, num2)
-    } else if(operate === '-') {
+    } else if(operator === '-') {
         return subtract(num1, num2)
-    } else if(operate === '*') {
+    } else if(operator === '*') {
         return multiply(num1, num2)
-    } else if(operate === '/') {
+    } else if(operator === '/') {
         return divide(num1, num2)
     }
 }
 
-function display() {
+function display(btn_text) {
+    let clickDisplay = document.querySelector('.clickDisplay')
+    let displayText = clickDisplay.textContent, n = displayText.length
+
+    if(btn_text === 'clear') {
+        clickDisplay.textContent = ''
+    }
+    else if(!isNaN(btn_text)) {
+        clickDisplay.textContent += btn_text
+    }
+    else if( (btn_text !== '=') && (displayText.length !== 0) ) {
+        if(isNaN(displayText[n-1])) {
+            clickDisplay.textContent = displayText.substring(0, n-1) + btn_text
+        } else {
+            clickDisplay.textContent += btn_text
+        }
+    }
+}
+
+function calculate() {
     let btns = document.querySelectorAll('.btn')
     btns.forEach((btn) => {
         btn.addEventListener('click', () => {
             let btn_text = btn.textContent
-            let clickDisplay = document.querySelector('.clickDisplay')
-            if(btn_text === 'clear') {
-                clickDisplay.textContent = ''
-            } else if(btn_text !== '=') {
-                clickDisplay.textContent += btn_text
-            }
+            display(btn_text)
         })
     })
 }
 
-display()
+calculate()
