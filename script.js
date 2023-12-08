2d4d89fbeb05fdd4892fb0248413dbf5f8bae0b2
vrushank <vrushank1729@gmail.com>
2023-12-08 23:00:58 +0530

    can handle negative integers too

--- script.js
+++ script.js
@@ -21,26 +21,94 @@ function calculate() {
             if(!isNaN(btn_text)) {
                 clickDisplay.textContent += btn_text
             }
+
             else if(btn_text === 'clear') {
                 clickDisplay.textContent = ''
             }
-            else if(btn_text !== '=' && (n>0) && !isNaN(displayText.substring(0, n-1))) {
-                if(isNaN(displayText[n-1])) {
-                    clickDisplay.textContent = displayText.substring(0, n-1) + btn_text
-                } else {
+
+            else if(btn_text === '-') {
+                if( n===1 && !isNaN(displayText[0]) ) {
                     clickDisplay.textContent += btn_text
                 }
+                else if(n===0 || n===2) {
+                    clickDisplay.textContent += btn_text
+                }
+                else if( n>2 && !isNaN(displayText.substring(0,n-1))) {
+                    clickDisplay.textContent += btn_text
+                }
+                else if( n>2 && !isNaN(displayText[n-1])) {
+                    let operators = ['+', '-', '*', '/']
+                    for(let i=0; i<4; i++) {
+                        let str = displayText.substring(1,n)
+                        if(str.includes(operators[i])) {
+                            let j = str.indexOf(operators[i]) + 1
+                            let num1 = parseInt( displayText.substring(0, j), 10 )
+                            let num2 = parseInt( displayText.substring(j+1, n), 10 )
+        
+                            let result = operate(num1, operators[i], num2)
+                            if(btn_text !== '=') {
+                                clickDisplay.textContent = result + btn_text
+                            } else {
+                                clickDisplay.textContent = result
+                            }
+                        }
+                    }
+                }
             }
-            else {
+
+            else if( ['+', '*', '/'].includes(btn_text) && (n>0) ) {
+                if( (n===1) && !isNaN(displayText[0]) ){
+                    clickDisplay.textContent += btn_text
+                }
+                else if( n==2 ) {
+                    if( !isNaN(displayText[1]) ) {
+                        clickDisplay.textContent += btn_text
+                    }
+                    if( !isNaN(displayText[0]) && isNaN(displayText[1]) ) {
+                        clickDisplay.textContent = displayText.substring(0, n-1) + btn_text
+                    }
+                }
+                else if( n>2 && !isNaN(displayText.substring(0, n-1)) ) {
+                    if(isNaN(displayText[n-1])) {
+                        clickDisplay.textContent = displayText.substring(0, n-1) + btn_text
+                    } else {
+                        clickDisplay.textContent += btn_text
+                    }
+                }
+                else if ( !isNaN(displayText[n-1]) ) {
+                    let operators = ['+', '-', '*', '/']
+                    for(let i=0; i<4; i++) {
+                        let str = displayText.substring(1,n)
+                        if(str.includes(operators[i])) {
+                            let j = str.indexOf(operators[i]) + 1
+                            let num1 = parseInt( displayText.substring(0, j), 10 )
+                            let num2 = parseInt( displayText.substring(j+1, n), 10 )
+    
+                            let result = operate(num1, operators[i], num2)
+                            if(btn_text !== '=') {
+                                clickDisplay.textContent = result + btn_text
+                            } else {
+                                clickDisplay.textContent = result
+                            }
+                        }
+                    }
+                }
+            }
+            else if( btn_text === '=' && !isNaN(displayText[n-1]) ) {
                 let operators = ['+', '-', '*', '/']
                 for(let i=0; i<4; i++) {
-                    if(displayText.includes(operators[i])) {
-                        let j = displayText.indexOf(operators[i])
+                    let str = displayText.substring(1,n)
+                    if(str.includes(operators[i])) {
+                        let j = str.indexOf(operators[i]) + 1
                         let num1 = parseInt( displayText.substring(0, j), 10 )
                         let num2 = parseInt( displayText.substring(j+1, n), 10 )
-
+    
                         let result = operate(num1, operators[i], num2)
-                        clickDisplay.textContent = result + btn_text
+                        if(btn_text !== '=') {
+                            clickDisplay.textContent = result + btn_text
+                        } else {
+                            clickDisplay.textContent = result
+                        }
                     }
                 }
             }
